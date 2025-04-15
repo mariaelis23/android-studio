7e1d6b307b1142a0998d213e9c1643f7ebc2b551
Maria Elis <126252661+mariaelis23@users.noreply.github.com>
2025-04-14 22:05:45 -0300

    Create treino.java

---
+++ treino.java
@@ -0,0 +1,17 @@
+//selecionar empty views activity
+
+//          I N T E R F A C E
+
+//primeiro o que deve ser feito é a tela, o que será visto pelo usuário, e depois os códigos
+//adicionar CONSTRAINTS em TUDO (puxar todas as setas da figura para os respectivos lados)
+//no campo "text" mudar o nome (nome que vai aparecer na tela), e mudar também as ID's (identificação, pra conseguir manipulá-las no código)
+
+//quando tiver mais de um texto, o texto de baixo, quando for adicionar a constraint, precisa ligar ela ate a parte em que aparece o outro texto, 
+//por exemplo, quando há dois textos um embaixo do outro, o texto de baixo deverá ter sua constraint de cima adicionada até a parte inferior da constraint do texto que está em cima
+
+//Plain Text > pra quando o usuário entra com uma informação. e só pode ser textos
+// dos textos, tudo o que está sublinhado é pro usuário entrar com texto
+
+//no texto, na opção "hint" vc pode adicionar um texto que será de visualização para o usuário, quando ele escrever, o texto some
+//na opção "inputType" você pode selecionar o tipo de caracter de entrada
+
